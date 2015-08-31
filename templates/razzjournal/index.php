9fe6a856485ce34cf2da1cf84cc389af692cf0e9
Andrew <neoandrew@yandex.ru>
2015-08-31 04:31:59 +0300

    the end of the development day

--- templates/razzjournal/index.php
+++ templates/razzjournal/index.php
@@ -1,6 +1,7 @@
 <?php 
 	defined( '_JEXEC' ) or die( 'Restricted access' );
 	$doc = JFactory::getDocument();
+	$this->addScript( '/templates/' . $this->template . '/js/collapsible.js', 'text/javascript');
 	$this->addStyleSheet( 'templates/' . $this->template . '/css/style.css' );
 	$this->addStyleSheet( 'templates/' . $this->template . '/css/screen.css' );
 	$this->addStyleSheet( 'templates/' . $this->template . '/css/print.css' );
@@ -12,13 +13,39 @@
    xml:lang="<?php echo $this->language; ?>" lang="<?php echo $this->language; ?>" >
 	<head>
 		<jdoc:include type="head" />
+
+		<!--WORK! WORK! WORK!-->
+
+		<!--<script type="text/javascript">
+			$(document).ready(function(){
+    			$('.collapsible').collapsible({
+     				accordion : false // A setting that changes the collapsible behavior to expandable instead of the default accordion style
+    			});
+  			});
+		</script>-->
+
+		<!--WORK! WORK! WORK!-->
+
 	</head>
 	<body>
 		<!--HEADER-->
 		<div id="header" class="row">
-			<div id="logotype" class="col s12 m6 l3 left">
+			<div id="logotype" class="col s12 m6 l3 left valign-wrapper">
+				<img class="valign" src="<?php echo $this->baseurl; ?>/templates/<?php echo $this->template; ?>/images/logo.gif">
+				<jdoc:include type="modules" name="logotype" /> 
 			</div><!--logotype.col-->
 			<div id="login"  class="col s12 m6 l3 right">
+
+			<!--WORK! WORK! WORK!-->
+				<!--<ul class="collapsible" data-collapsible="accordion">
+    				<li>
+      					<div class="collapsible-header"><i class="material-icons">filter_drama</i>First</div>
+      					<div class="collapsible-body"><p>Lorem ipsum dolor sit amet.</p></div>
+    				</li>
+    			</ul>
+				<jdoc:include type="modules" name="login" />-->
+			<!--WORK! WORK! WORK!-->
+
 			</div><!--login.col-->		
 		</div><!--header.row-->
 		<!--END OF HEADER-->
@@ -26,20 +53,27 @@
 		<div id="main" class="row">
 		<div id="aside" class="col s12 m4 l3 left">	
 			<div id="menu">
+				<jdoc:include type="modules" name="menu" /> 
 			</div><!--menu-->	
 			<div id="search">
+				<jdoc:include type="modules" name="search" /> 
 			</div><!--search-->		
 		</div><!--aside.col-->
 		<div id="content" class="col s12 m8 l9 right">
-			<div class="breadcrumbs">
+			<div id="breadcrumbs">
+				<jdoc:include type="modules" name="breadcrumbs" /> 
 			</div><!--breadcrumbs-->	
 			<div id="article">
+				<jdoc:include type="modules" name="content" />
+				<jdoc:include type="component" />
 			</div><!--article-->
 		</div><!--content.col-->
 		</div><!--main.row-->
 		<!--END OF MAIN-->
 		<!--FOOTER-->
-		<div id="footer" class="row">			
+		<div id="footer" class="row">
+			<jdoc:include type="modules" name="footer" /> 	
+			<jdoc:include type="modules" name="debug" /> 		
 		</div><!--footer.row-->
 		<!--END OF FOOTER-->
 	</body>
